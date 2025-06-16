668ea90c1cff1b94a48e8795870b9c034e31c89e
daptor <christorfu@gmail.com>
2025-06-16 18:43:48 -0400

    Add files via upload
    123321

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-import { supabase } from './supabaseClient.js';
+// Inicializa el cliente de Supabase con tus credenciales (reemplaza con tus valores reales)
+const supabase = window.supabase.createClient('https://mxqrzhpyfwuutardehyu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14cXJ6aHB5Znd1dXRhcmRlaHl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0NjE0NDUsImV4cCI6MjA1OTAzNzQ0NX0.JaXYgxWKcbI_b7z0-ihvEHuueU7SSSy-_LlJfiYS9xs');
+
 
 // *****************Función para actualizar el contador global en Supabase*******************
 async function actualizarContadorSupabase() {

06a52934c0ea2c650ebe6e1ed297dac5949b44ad
daptor <christorfu@gmail.com>
2025-12-28 00:41:23 -0300

    1 ok

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Inicializa SupabaseClient (UNA SOLA VEZ)
-const supabaseclientClient = window.supabaseclient.createClient(
+const supabaseclientClient = supabaseclient.createClient(
   'https://mxqrzhpyfwuutardehyu.supabaseclient.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14cXJ6aHB5Znd1dXRhcmRlaHl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0NjE0NDUsImV4cCI6MjA1OTAzNzQ0NX0.JaXYgxWKcbI_b7z0-ihvEHuueU7SSSy-_LlJfiYS9xs'
 );

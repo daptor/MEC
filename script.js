b0e60ae388e9d5ba511bad1aa8f98f511f501657
daptor <christorfu@gmail.com>
2026-06-19 01:24:22 -0400

    1

--- script.js
+++ script.js
@@ -2091,7 +2091,7 @@ const AS_ALLOWED_MIME = [
   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
   "application/vnd.ms-excel",
   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-  "application/vnd.ms-excel.sheet.macroenabled.12",
+  "application/vnd.ms-excel.sheet.macroEnabled.12", // 👈 EXACTO como file.type
   "image/jpeg",
   "image/png"
 ];
